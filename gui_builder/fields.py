c609cab6f33ef27022b89ff053fb87c3827e9689
Christopher Toth <q.alpha@gmail.com>
2012-04-29 04:48:31 -0400

    Added IntText field

--- gui_builder/fields.py
+++ gui_builder/fields.py
@@ -56,6 +56,9 @@ class GUIField(object):
 class Text(GUIField):
  widget_type = widgets.Text
 
+class IntText(GUIField):
+ widget_type = widgets.IntText
+
 class Button(GUIField):
  widget_type = widgets.Button
 
