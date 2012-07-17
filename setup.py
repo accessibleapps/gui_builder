dd72937247355a6d0bc001aec2f701737368619a
Christopher Toth <q.alpha@gmail.com>
2012-07-16 22:31:45 -0400

    Added setup.py with bumped version.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+__version__ = 0.02
+__doc__ = """Declarative GUIs"""
+
+setup(
+ name = "gui_builder",
+ version = __version__,
+ description = __doc__,
+ packages = find_packages(),
+ classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Topic :: Software Development :: Libraries',
+ ],
+)
