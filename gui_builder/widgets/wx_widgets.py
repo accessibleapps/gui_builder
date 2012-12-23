3109835f51e82f5a53cb3aabe9b685edf5fe23e0
Christopher Toth <q.alpha@gmail.com>
2012-12-22 22:07:21 -0700

    Slightly different callback mechanism

--- gui_builder/widgets/wx_widgets.py
+++ gui_builder/widgets/wx_widgets.py
@@ -313,7 +313,8 @@ class ListBox(ChoiceWidget):
 class ListView(ChoiceWidget):
  control_type = wx.ListView
  style_prefix = "LC"
- default_callback_type = "LIST_ITEM_FOCUSED"
+ event_prefix = 'EVT_LIST'
+ default_callback_type = "ITEM_FOCUSED"
 
  def __init__(self, choices=None, **kwargs):
   super(ListView, self).__init__(**kwargs)
