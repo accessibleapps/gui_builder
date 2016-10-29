15d972f4dc0c6c1e7eb42a90123efb4ccdf23ed8
Christopher Toth <q.alpha@gmail.com>
2016-10-28 20:59:54 -0600

    spaces2tabs on setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ __version__ = 0.25
 __doc__ = """Declarative GUIs"""
 
 setup(
- name = "gui_builder",
- version = __version__,
- description = __doc__,
- packages = find_packages(),
- classifiers = [
-  'Development Status :: 3 - Alpha',
-  'Intended Audience :: Developers',
-  'Programming Language :: Python',
-  'Topic :: Software Development :: Libraries',
- ],
+	name = "gui_builder",
+	version = __version__,
+	description = __doc__,
+	packages = find_packages(),
+	install_requires = ['six', ],
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries',
+	],
 )
