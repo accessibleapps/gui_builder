647941d2a11ccf191ab96167dab8a86d22c0a019
Christopher Toth <q.alpha@gmail.com>
2022-04-08 22:14:27 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = 0.25
+__version__ = "0.26"
 __doc__ = """Declarative GUIs"""
 
 setup(
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(),
     install_requires=["six"],
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries",
